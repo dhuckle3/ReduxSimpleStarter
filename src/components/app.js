f7ac715e5cc79fb429de0ef1d3300db67974f2c1
Donald Huckle <dhuckle3@gmail.com>
2017-01-30 20:35:29 -0500

    making filenames uniform; using _ instead of -

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import SearchBar from '../containers/search-bar';
-import StationList from './station-list';
+import SearchBar from '../containers/search_bar';
+import StationList from './station_list';
 
 export default class App extends Component {
   render() {
