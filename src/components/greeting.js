486e685d8223dcbadad76e7ccafd11e286e57723
Donald Huckle <dhuckle3@gmail.com>
2017-02-15 20:06:14 -0500

    add an index route which loads the stub post index

--- src/components/greeting.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Greeting extends Component {
-    render() {
-        return <div>Hello, World!</div>;
-    };
-}
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-import Greeting from './components/greeting';
+import PostIndex from './components/posts_index';
 
 export default (
     <Route path="/" component={App}>
-        <Route path="greet" component={Greeting} />
+        <IndexRoute component={PostIndex} />
     </Route>
 );
